33ce3bf48a261ef953031f9eafbd69f123c1b9c5
juege <1402272093@qq.com>
2024-12-19 16:28:16 +0800

    init

---
+++ src/main/java/com/juege/tech_doc/TechDocApplication.java
@@ -0,0 +1,13 @@
+package com.juege.tech_doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechDocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juege/tech_doc/TechDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juege.tech_doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
