9f144898ab4776093c3d5ca4f407a4cebcb21c4e
juege <1402272093@qq.com>
2025-01-12 23:22:02 +0800

    change pom.xml+test.http+TestController+application.properties+all.sql

---
+++ src/main/java/com/juege/tech_doc/domain/TestTable.java
@@ -0,0 +1,59 @@
+package com.juege.tech_doc.domain;
+
+import java.util.Date;
+
+public class TestTable {
+    private Integer id;
+
+    private String name;
+
+    private Date createdAt;
+
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", createdAt=").append(createdAt);
+        sb.append(", description=").append(description);
+        sb.append("]");
+        return sb.toString();
+    }
+}
