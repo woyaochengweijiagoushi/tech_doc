9e3b955961514a782936b9873589a11285b01ad0
juege <1402272093@qq.com>
2025-02-12 16:01:55 +0800

    新增分类管理 电子书管理 文档管理

--- src/main/java/com/juege/tech_doc/TechDocApplication.java
+++ src/main/java/com/juege/tech_doc/TechDocApplication.java
@@ -1,12 +1,20 @@
 package com.juege.tech_doc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@ComponentScan("com.juege")
 @SpringBootApplication
+@MapperScan("com.juege.tech_doc.mapper")
+@EnableScheduling
+@EnableAsync
 public class TechDocApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(TechDocApplication.class);
---
+++ web/src/views/about.vue
@@ -0,0 +1,58 @@
+<template>
+  <a-layout>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      <div class="about">
+        <h2>我是觉哥，7年Java全栈+大数据资深开发</h2>
+        <div><b>专注Java全栈+大数据技术分享</b></div>
+        <div>
+          <b>项目实战：
+            <a href="baidu.com" target="_blank">
+            《xxx系统》
+            </a>
+          </b>
+        </div>
+        <div>
+          <b>项目实战：
+            <a href="baidu.com" target="_blank">
+            《xxx课程系统》
+            </a>
+          </b>
+        </div>
+        <div>
+          <b>免费课程：
+            <a href="baidu.com" target="_blank">
+            《xxx教程》
+            </a>
+          </b>
+        </div>
+        <br>
+        <div><b>QQ：xxx，<span style="color: rgb(194, 79, 74)">服务器使用咨询、购买优惠</span>，加QQ，备注</b></div>
+        <div><b>微信号：xxx</b></div>
+        <div><b>QQ群：xxx，Java全栈交流群</b></div>
+        <div><b>个人全栈技术博客：
+          <a href="http://www.xxx.com" target="_blank">xxx</a></b>
+        </div>
+        <div style="color: rgb(194, 79, 74)"><b>公众号：xxx，扫码关注</b></div>
+        <div>
+          <img src="/image/xxxquanzhan.jpg">
+        </div>
+      </div>
+    </a-layout-content>
+  </a-layout>
+</template>
+
+<style>
+  .about {
+    line-height: 30px;
+    padding: 40px;
+    background-color: #f5f5f5;
+    font-size: 16px;
+  }
+  .about h2 {
+    margin-bottom: 30px;
+    font-weight: 700;
+  }
+  .about img {
+    margin-top: 10px;
+  }
+</style>
