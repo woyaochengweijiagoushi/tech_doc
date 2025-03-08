1b7c408b0bf3d71d5fc114085066688f068966c5
syd <syidong@aliyun.com>
2025-03-08 22:45:59 +0800

    feature: 优化以及添加支付功能

---
+++ src/main/java/com/juege/tech_doc/mapper/TdPayRecordMapper.java
@@ -0,0 +1,18 @@
+package com.juege.tech_doc.mapper;
+
+import com.juege.tech_doc.domain.TdPayRecord;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * 支付记录表 Mapper 接口
+ * </p>
+ *
+ * @author syd
+ * @since 2025-03-08
+ */
+@Mapper
+public interface TdPayRecordMapper extends BaseMapper<TdPayRecord> {
+
+}
